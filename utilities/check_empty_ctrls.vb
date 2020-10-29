1ffb00f87c4fec37d3c2e8acb04c0027192f88d2
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-10-28 22:36:01 -0400

    Update check_empty_ctrls.vb

--- utilities/check_empty_ctrls.vb
+++ utilities/check_empty_ctrls.vb
@@ -3,12 +3,14 @@ Function ValidateInput() As Boolean
 
     Dim Ctrl As Control
     For Each Ctrl In Me.Controls
+        'check text box
         If TypeName(Ctrl) = "TextBox" Then
             If Ctrl.Text = vbNullString Then
                 EmptyInputsFound = True
                 Exit For
             End If
         End If
+        'check combo box
         If TypeName(Ctrl) = "ComboBox" Then
             If Ctrl.Text = vbNullString Then
                 EmptyInputsFound = True
